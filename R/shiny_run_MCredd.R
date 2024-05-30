6069a1db7b605bd40198cfc26b182bc262bc06f2
gaelso <gaelsola@hotmail.fr>
2024-05-30 08:55:56 +0200

    made barebone work without i18n

--- R/shiny_run_MCredd.R
+++ R/shiny_run_MCredd.R
@@ -1,31 +1,52 @@
-# green: rgba(68,141,182);
-# blue: rgba(119,171,22);
-# font: mono
-
-
+#' Run Monte Carlo for REDD+ uncertainty's shiny app
+#'
+#' @description Starts a R Shiny application that guide users to fill in context and
+#'              land use changes for forest related greenhouse gas emissions and removals.
+#'              Inputs include distribution parameters for various distribution then the app
+#'              returns error propagation and sensitivity analysis of the overall uncertainty
+#'              for GHG estimates.
+#'
+#' @param ... arguments to pass to shinyApp
+#'
+#' @import shiny
+#' @import bslib
+#'
+#'
+#' @examples
+#' if (interactive()) {
+#'
+#' shiny_run_MCredd()
+#'
+#' }
+#'
+#' @export
 shiny_run_MCredd <- function(...) {
 
   ## GLOBAL #################################################################
+
+  ## Initiate translation
+  # i18n <- shiny.i18n::Translator$new(translation_csvs_path = 'assets/translation')
+  # i18n$set_translation_language('en')
+  #
   language_selector2 <- shinyWidgets::pickerInput(
     inputId = "language",
     label = NULL,
-    choices = c("en", "te"),
-    choicesOpt =  list(content = c('<i class="fi fi-gb"></i> EN', '<i class="fi fi-tl"></i> TL')),
+    choices = c("en", "fr"),
+    choicesOpt =  list(content = c('<i class="fi fi-gb"></i> EN', '<i class="fi fi-fr"></i> FR')),
     selected = "en",
     width = "auto",
-    option = pickerOptions(style = "z-index:10000;")
+    option = shinyWidgets::pickerOptions(style = "z-index:10000;")
   )
 
-
   ## UI #####################################################################
-  ui <- tagList(
+  ui <- shiny::tagList(
 
     ## Setup ---------------------------------------------------------------------
     # shiny::withMathJax(),
     # shinyjs::useShinyjs(),
     # shinyWidgets::useSweetAlert(),
-    shiny.i18n::usei18n(i18n),
-    tags$head(tags$link(rel = "stylesheet", type = "text/css", href = "style.css")),
+    # shiny.i18n::usei18n(i18n),
+    # tags$head(tags$link(rel = "stylesheet", type = "text/css", href = "style.css")),
     htmltools::htmlDependency(
       name = "flag-icons",
       version = "6.6.6",
@@ -42,7 +63,8 @@ shiny_run_MCredd <- function(...) {
     page_navbar(
       id = "navbar",
       ## ++ Styling ++++++
-      title = div(img(img(src="assets/Arena-logo.png", width = '100%')), i18n$t("Timor Leste REDD+ Geoportal"), style = "display:inline;"),
+      #title = div(img(src="assets/Arena-Logo.png", width = '100%'), i18n$t("Timor Leste REDD+ Geoportal"), style = "display:inline;"),
+      title = div(img(src="assets/Arena-Logo.png", width = '100%'), "Timor Leste REDD+ Geoportal", style = "display:inline;"),
       window_title = "TL REDD+ Geoportal",
       theme = bs_theme(
         version = 5,
@@ -50,15 +72,16 @@ shiny_run_MCredd <- function(...) {
         base_font = font_google("Noto Sans", wght = c(400, 700)),
         code_font = font_google("Fira Code"),
         heading_font = font_google("Lato", wght = 700),
-        primary = rgb(68,141,182),
-        secondary =  rgba(119,171,22)
+        primary = rgb(68,141,182, maxColorValue = 255),
+        secondary = rgb(119,171,22, maxColorValue = 255),
       ),
       fillable = "portal",
       bg = "#f8f9fa",
 
       ## ++ Panels +++++
       nav_panel(
-        title = i18n$t("Home"),
+        #title = i18n$t("Home"),
+        title = "Home",
         value = "home",
         icon = icon("campground"),
         #mod_home_UI("tab_home") ## See R/mod_home_UI.R
@@ -69,7 +92,7 @@ shiny_run_MCredd <- function(...) {
       nav_item(language_selector2)
 
     ) |> ## End page_navbar
-      tagAppendAttributes(.cssSelector = "nav", class = "navbar-expand-lg")
+      shiny::tagAppendAttributes(.cssSelector = "nav", class = "navbar-expand-lg")
   ) ## End tagList
 
 
@@ -88,15 +111,15 @@ shiny_run_MCredd <- function(...) {
 
 
     ## + Module server functions ============================================
-    mod_home_server("tab_home", rv = rv)
-
-    mod_CV_server("tab_cv", rv = rv)
-
-    mod_time_server("tab_time", rv = rv)
-
-    mod_opti_server("tab_opti", rv = rv)
-
-    mod_results_server("tab_res", rv = rv)
+    # mod_home_server("tab_home", rv = rv)
+    #
+    # mod_CV_server("tab_cv", rv = rv)
+    #
+    # mod_time_server("tab_time", rv = rv)
+    #
+    # mod_opti_server("tab_opti", rv = rv)
+    #
+    # mod_results_server("tab_res", rv = rv)
 
 
 
@@ -122,4 +145,4 @@ shiny_run_MCredd <- function(...) {
   ## App call ###############################################################
   shinyApp(ui, server, ...)
 
-} ## END function shiny_optimize_NFI()
+} ## END function

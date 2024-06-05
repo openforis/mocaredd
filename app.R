1adfb3ea8f281ed58362b448a4746b1d12a51eb2
gaelso <gaelsola@hotmail.fr>
2024-06-05 11:07:04 +0200

    correct typo

--- app.R
+++ app.R
@@ -3,4 +3,4 @@
 # Or use the blue button on top of this file
 
 pkgload::load_all(export_all = FALSE, helpers = FALSE, attach_testthat = FALSE)
-oNFI::shiny_optimize_NFI()
+MCredd::shiny_run_MCredd()

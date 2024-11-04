4cd785b706428112a4f54f55685a18192bae14ef
gaelso <gaelsola@hotmail.fr>
2024-11-05 00:18:14 +0100

    Started writing proper tests

---
+++ tests/testthat/test-fct_check_data.R
@@ -0,0 +1,18 @@
+
+# library(testthat)
+# library(mocaredd)
+
+
+cs <- readxl::read_xlsx(system.file("extdata/example1.xlsx", package = "mocaredd"), sheet = "c_stock", na = "NA")
+ad <- readxl::read_xlsx(system.file("extdata/example1.xlsx", package = "mocaredd"), sheet = "AD_lu_transitions", na = "NA")
+
+init <- list(
+  c_pools = c("AGB", "BGB", "RS", "DW", "LI", "SOC", "ALL", "DG_ratio"),
+  redd_acti = c("DF", "DG", "EN", "EN_AF", "EN_RE")
+)
+
+
+
+test_that("all checks pass", {
+  expect_equal(all(fct_check_data(.ad = ad, .cs = cs, .init = init)), TRUE)
+})

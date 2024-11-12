0f8a2811158281eff837ea0562f7b3b07b2bf78d
gaelso <gaelsola@hotmail.fr>
2024-11-12 16:25:34 +0100

    done 4 / 6 checks in function anmd adapted template

--- R/fct_check_data2.R
+++ R/fct_check_data2.R
@@ -41,175 +41,208 @@
 #'   )
 #'
 #' app_checklist <- list(
-#'   xlsx_tabs  = c("user_inputs", "time_periods", "AD_lu_transitions", "c_stock"),
-#'   cat_cunits = c("DM", "C"),
-#'   cat_cpools = c("AGB", "BGB", "DW", "LI", "SOC", "ALL"),
-#'   cat_racti  = c("DF", "DG", "EN", "EN_AF", "EN_RE"),
-#'   cat_ptype  = c("REF", "M"),
-#'   col_usr    = c("trunc_pdf", "n_iter", "ran_seed", "c_unit", "c_fraction", "dg_pool", "dg_expool", "ad_annual", "conf_level"),
-#'   col_time   = c("period_no", "year_start", "year_end", "period_type"),
-#'   col_ad     = c("trans_no",	"trans_id",	"trans_period",	"redd_activity", "lu_initial_id", "lu_initial",	"lu_final_id", "lu_final", "trans_area", "trans_se", "trans_pdf", "trans_pdf_a", "trans_pdf_b", "trans_pdf_c"),
-#'   col_cs     = c("c_no",	"c_id",	"lu_id", "lu_name",	"c_pool",	"c_value",	"c_se",	"c_pdf",	"c_pdf_a",	"c_pdf_b",	"c_pdf_c")
-#'   )
+#' xlsx_tabs  = c("user_inputs", "time_periods", "AD_lu_transitions", "c_stocks"),
+#' cat_cunits = c("DM", "C"),
+#' cat_cpools = c("AGB", "BGB", "DW", "LI", "SOC", "ALL"),
+#' cat_racti  = c("DF", "DG", "EN", "EN_AF", "EN_RE"),
+#' cat_ptype  = c("REF", "REF[0-9]", "MON", "MON[0-9]"),
+#' cat_pdf    = c("normal", "beta"),
+#' col_usr    = c("trunc_pdf", "n_iter", "ran_seed", "c_unit", "c_fraction", "dg_pool", "dg_expool", "ad_annual", "conf_level"),
+#' col_time   = c("period_no", "year_start", "year_end", "period_type"),
+#' col_ad     = c("trans_no",	"trans_id",	"trans_period",	"redd_activity", "lu_initial_id", "lu_initial",	"lu_final_id", "lu_final", "trans_area", "trans_se", "trans_pdf", "trans_pdf_a", "trans_pdf_b", "trans_pdf_c"),
+#' col_cs     = c("c_no",	"c_id",	"lu_id", "lu_name",	"c_pool",	"c_value",	"c_se",	"c_pdf",	"c_pdf_a",	"c_pdf_b",	"c_pdf_c")
+#' )
+#'
+#' app_checklist$cat_cpools_all <- c(app_checklist$c_pools, "RS", "DG_ratio")
 #'
 #' fct_check_data(.ad = ad, .cs = cs, .usr = usr, .time = time, .checklist = app_checklist)
 #'
 #' @export
 fct_check_data2 <- function(.ad, .cs, .usr, .time, .checklist){
 
+  tmp <- list()
+  out <- list()
+
   ## Check 1. tables have at least the correct columns
-  out$cols_usr_ok  <- .checklist$col_usr %in% names(.usr)
-  out$cols_time_ok <- .checklist$col_time %in% names(.time)
-  out$cols_ad_ok   <- .checklist$col_ad %in% names(.ad)
-  out$cols_cs_ok   <- .checklist$col_cs %in% names(.cs)
+  tmp$cols_usr_ok  <- all(.checklist$col_usr %in% names(.usr))
+  tmp$cols_time_ok <- all(.checklist$col_time %in% names(.time))
+  tmp$cols_ad_ok   <- all(.checklist$col_ad %in% names(.ad))
+  tmp$cols_cs_ok   <- all(.checklist$col_cs %in% names(.cs))
 
   out$cols_ok <- all(
-    out$cols_usr_ok, out$cols_time_ok, out$cols_ad_ok, out$cols_cs_ok
+    tmp$cols_usr_ok, tmp$cols_time_ok, tmp$cols_ad_ok, tmp$cols_cs_ok
     )
 
   if (out$cols_ok) {
-    out$cols_pb <- NULL
+    out$cols_pb <- ""
   } else {
-    out$cols_usr_pb  <- ifelse(out$cols_usr_ok, NULL, "user_inputs")
-    out$cols_time_pb <- ifelse(out$cols_time_ok, NULL, "time_periods")
-    out$cols_ad_pb   <- ifelse(out$cols_ad_ok, NULL, "AD_lu_transitions")
-    out$cols_cs_pb   <- ifelse(out$cols_cs_ok, NULL, "c_stocks")
+    tmp$cols_usr_pb  <- ifelse(tmp$cols_usr_ok, NULL, "user_inputs")
+    tmp$cols_time_pb <- ifelse(tmp$cols_time_ok, NULL, "time_periods")
+    tmp$cols_ad_pb   <- ifelse(tmp$cols_ad_ok, NULL, "AD_lu_transitions")
+    tmp$cols_cs_pb   <- ifelse(tmp$cols_cs_ok, NULL, "c_stocks")
 
-    out$cols_pb <- c(out$cols_usr_pb, out$cols_time_pb, out$cols_ad_pb, out$cols_cs_pb)
+    out$cols_pb <- c(tmp$cols_usr_pb, tmp$cols_time_pb, tmp$cols_ad_pb, tmp$cols_cs_pb)
   }
 
   ## Check 2. tables dimensions
-  out$size_usr_ok <- nrow(.usr) == 1 ## usr has only one row
-  out$size_time_ok <- nrow(.time) >= 2 ## at least one ref and one monitoring
-  out$size_ad_ok <- nrow(.ad) >= 2 ## at least one lu transition for ref and monitoring
-  out$size_time_ok <- nrow(.cs) >= 2 ## at least one initial and one final cstock of one pool
+  tmp$size_usr_ok <- nrow(.usr) == 1 ## usr has only one row
+  tmp$size_time_ok <- nrow(.time) >= 2 ## at least one ref and one monitoring
+  tmp$size_ad_ok <- nrow(.ad) >= 2 ## at least one lu transition for ref and monitoring
+  tmp$size_cs_ok <- nrow(.cs) >= 2 ## at least one initial and one final cstock of one pool
 
   out$size_ok <- all(
-    out$size_usr_ok, out$size_time_ok, out$size_ad_ok, out$size_time_ok
+    out$size_usr_ok, out$size_time_ok, out$size_ad_ok, out$size_cs_ok
   )
 
   if (out$size_ok) {
-    out$size_pb <- NULL
+    out$size_pb <- ""
   } else {
-    out$size_usr_pb  <- ifelse(out$size_usr_ok, NULL, "user_inputs")
-    out$size_time_pb <- ifelse(out$size_time_ok, NULL, "time_periods")
-    out$size_ad_pb   <- ifelse(out$size_ad_ok, NULL, "AD_lu_transitions")
-    out$size_cs_pb   <- ifelse(out$size_cs_ok, NULL, "c_stocks")
+    tmp$size_usr_pb  <- ifelse(tmp$size_usr_ok, NULL, "user_inputs")
+    tmp$size_time_pb <- ifelse(tmp$size_time_ok, NULL, "time_periods")
+    tmp$size_ad_pb   <- ifelse(tmp$size_ad_ok, NULL, "AD_lu_transitions")
+    tmp$size_cs_pb   <- ifelse(tmp$size_cs_ok, NULL, "c_stocks")
 
-    out$size_pb <- c(out$size_usr_pb, out$size_time_pb, out$size_ad_pb, out$size_cs_pb)
+    tmp$size_pb <- c(tmp$size_usr_pb, tmp$size_time_pb, tmp$size_ad_pb, tmp$size_cs_pb)
   }
 
 
   ## Check 3. data types are correct
   ## - usr tab
-  out$usr_datatypes_ok <- all(
+  tmp$datatypes_usr_ok <- all(
     is.logical(.usr$trunc_pdf),
-    is.integer(.usr$n_iter),
-    is.integer(.usr$ran_seed) | is.na(.usr$ran_seed),
+    is.numeric(.usr$n_iter),
+    is.numeric(.usr$ran_seed) | is.logical(.usr$ran_seed),
     is.character(.usr$c_unit),
     #is.numeric(.usr$c_fraction), ## !!! C_fraction needs mean, sd if not NA, maybe better to have it in Cstock table
-    is.character(.usr$dg_pool) | is.na(.usr$dg_pool),
-    is.character(.usr$dg_expool) | is.na(.usr$dg_expool),
+    is.character(.usr$dg_pool) | is.logical(.usr$dg_pool),
+    is.logical(.usr$dg_expool),
     is.logical(.usr$ad_annual),
     is.numeric(.usr$conf_level)
   )
 
   ## - time tab
-  out$time_datatypes_ok <- all(
-    is.character(period_no),
-    is.integer(year_start),
-    is.integer(year_end),
-    is.character(period_type)
+  tmp$datatypes_time_ok <- all(
+    is.character(.time$period_no),
+    is.numeric(.time$year_start),
+    is.numeric(.time$year_end),
+    is.character(.time$period_type)
   )
 
   ## - ad tab
-  out$ad_datatypes_ok <- all(
-    is.integer(trans_no),
-    is.character(trans_id),
-    is.character(trans_period),
-    is.character(redd_activity),
-    is.character(lu_initial_id),
-    is.character(lu_initial),
-    is.character(lu_final_id),
-    is.character(lu_final),
-    is.numeric(trans_area), ## Should be is.integer() but might be too restrictive
-    is.numeric(trans_se),
-    is.character(trans_pdf),
-    is.numeric(trans_pdf_a),
-    is.numeric(trans_pdf_b),
-    is.numeric(trans_pdf_c)
+  tmp$datatypes_ad_ok <- all(
+    is.numeric(.ad$trans_no),
+    is.character(.ad$trans_id),
+    is.character(.ad$trans_period),
+    is.character(.ad$redd_activity),
+    is.character(.ad$lu_initial_id),
+    is.character(.ad$lu_initial),
+    is.character(.ad$lu_final_id),
+    is.character(.ad$lu_final),
+    is.numeric(.ad$trans_area) | is.logical(.ad$trans_area),
+    is.numeric(.ad$trans_se) | is.logical(.ad$trans_se),
+    is.character(.ad$trans_pdf),
+    is.numeric(.ad$trans_pdf_a) | is.logical(.ad$trans_pdf_a),
+    is.numeric(.ad$trans_pdf_b) | is.logical(.ad$trans_pdf_b),
+    is.numeric(.ad$trans_pdf_c) | is.logical(.ad$trans_pdf_c)
   )
 
   ## - cs tab
-  out$cs_datatypes_ok <- all(
-    is.integer(c_no),
-    is.character(c_id),
-    is.character(lu_id),
-    is.character(lu_name),
-    is.character(c_pool),
-    is.numeric(c_value),
-    is.numeric(c_se),
-    is.character(c_pdf),
-    is.numeric(c_pdf_a),
-    is.numeric(c_pdf_b),
-    is.numeric(c_pdf_c)
+  tmp$datatypes_cs_ok <- all(
+    is.numeric(.cs$c_no),
+    is.character(.cs$c_id),
+    is.character(.cs$lu_id),
+    is.character(.cs$lu_name),
+    is.character(.cs$c_pool),
+    is.numeric(.cs$c_value) | is.logical(.cs$c_value),
+    is.numeric(.cs$c_se) | is.logical(.cs$c_se),
+    is.character(.cs$c_pdf),
+    is.numeric(.cs$c_pdf_a) | is.logical(.cs$c_pdf_a),
+    is.numeric(.cs$c_pdf_b) | is.logical(.cs$c_pdf_b),
+    is.numeric(.cs$c_pdf_c) | is.logical(.cs$c_pdf_c)
   )
 
   ## - all
   out$datatypes_ok <- all(
-    out$usr_datatypes_ok,
-    out$time_datatypes_ok,
-    out$ad_datatypes_ok,
-    out$cs_datatypes_ok
+    tmp$datatypes_usr_ok,
+    tmp$datatypes_time_ok,
+    tmp$datatypes_ad_ok,
+    tmp$datatypes_cs_ok
   )
 
   if (out$datatypes_ok) {
-    out$datatypes_pb <- NULL
+    out$datatypes_pb <- ""
   } else {
-    out$datatypes_usr_pb  <- ifelse(out$datatypes_usr_ok, NULL, "user_inputs")
-    out$datatypes_time_pb <- ifelse(out$datatypes_time_ok, NULL, "time_periods")
-    out$datatypes_ad_pb   <- ifelse(out$datatypes_ad_ok, NULL, "AD_lu_transitions")
-    out$datatypes_cs_pb   <- ifelse(out$datatypes_cs_ok, NULL, "c_stocks")
+    tmp$datatypes_usr_pb  <- ifelse(tmp$datatypes_usr_ok, NULL, "user_inputs")
+    tmp$datatypes_time_pb <- ifelse(tmp$datatypes_time_ok, NULL, "time_periods")
+    tmp$datatypes_ad_pb   <- ifelse(tmp$datatypes_ad_ok, NULL, "AD_lu_transitions")
+    tmp$datatypes_cs_pb   <- ifelse(tmp$datatypes_cs_ok, NULL, "c_stocks")
 
-    out$datatypes_pb <- c(out$datatypes_usr_pb, out$datatypes_time_pb, out$datatypes_ad_pb, out$datatypes_cs_pb)
+    out$datatypes_pb <- c(tmp$datatypes_usr_pb, tmp$datatypes_time_pb, tmp$datatypes_ad_pb, tmp$datatypes_cs_pb)
   }
 
   ## Check 4. category variables are correct
+  ## - usr
   ## Get usr$dg_pool as vector
   ## ex. c("AGB", "BGB", "DW") %in% c("AGB", "BGB", "DW", "LI", "SOC", "ALL")
   dg_pool <- stringr::str_split(.usr$dg_pool, pattern = ",") |>
     purrr::map(stringr::str_trim) |>
     unlist()
 
-  ### Check usr categorie variables
-  out$usr_cats_ok <- all(
+  tmp$cats_usr_ok <- all(
     .usr$c_unit %in% .checklist$cat_cunits,
     all(dg_pool %in% .checklist$cat_cpools)
   )
 
-  ## Check time categorie variables
-  out$time_cats_ok <- all()
+  ## - time
+  ptype_pattern <- paste0(.checklist$cat_ptype, collapse = "|")
 
-  ## Check ad categorie variables
-  out$ad_cats_ok <- all()
-
-  ## Check cs categorie variables
-  out$cs_cats_ok <- all()
-
-  ## Combine
-  out$cats_ok <- all(
-    out$usr_cats_ok,
-    out$time_cats_ok,
-    out$ad_cats_ok,
-    out$cs_cats_ok
+  tmp$cats_time_ok <- all(
+     stringr::str_detect(unique(.time$period_type), pattern = ptype_pattern)
   )
 
-  ## +++ Check Unique IDs ----
+  ## - ad
+  tmp$cats_ad_ok <- all(
+    unique(.ad$redd_activity) %in% .checklist$cat_racti,
+    unique(.ad$trans_pdf) %in% .checklist$cat_pdf
+  )
+
+  ## - cs
+  tmp$cats_cs_ok <- all(
+    unique(.cs$c_pool) %in% .checklist$cat_cpools_all,
+    unique(.cs$c_pdf) %in% .checklist$cat_pdf
+  )
+
+  ## - Combine
+  out$cats_ok <- all(
+    tmp$cats_usr_ok,
+    tmp$cats_time_ok,
+    tmp$cats_ad_ok,
+    tmp$cats_cs_ok
+  )
+
+  if (out$cats_ok) {
+    out$cats_pb <- ""
+  } else {
+    tmp$cats_usr_pb  <- ifelse(tmp$cats_usr_ok, NULL, "user_inputs")
+    tmp$cats_time_pb <- ifelse(tmp$cats_time_ok, NULL, "time_periods")
+    tmp$cats_ad_pb   <- ifelse(tmp$cats_ad_ok, NULL, "AD_lu_transitions")
+    tmp$cats_cs_pb   <- ifelse(tmp$cats_cs_ok, NULL, "c_stocks")
+
+    out$cats_pb <- c(tmp$cats_usr_pb, tmp$cats_time_pb, tmp$cats_ad_pb, tmp$cats_cs_pb)
+  }
+
+
+  ## 5. Check Unique IDs
   out$ids_ok <- all()
 
-  ## +++ Check matching variables ----
+  ## 6. Check matching and logical interactions
+  # nb_ref <- .time |> dplyr::filter(stringr::str_detect(period_type, pattern = "REF|REF[0-9]"))
+  # nb_mon <- .time |> dplyr::filter(stringr::str_detect(period_type, pattern = "MON|MON[0-9]"))
+
+
   out$matches_ok <- all()
 
+  ## - combine all
   out$all_ok <- all(
     out$cols_ok,
     out$size_ok,

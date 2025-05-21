5a3007151001b39b4980516c25047b3c8f1a8063
gaelso <gaelsola@hotmail.fr>
2025-05-21 23:05:02 +0200

    Change test to template 2

--- tests/testthat/test-fct_combine_mcs_cstock.R
+++ tests/testthat/test-fct_combine_mcs_cstock.R
@@ -1,9 +1,9 @@
 
+library(mocaredd)
 library(readxl)
 library(dplyr)
-library(mocaredd)
 
-path <- system.file("extdata/example1-4pools.xlsx", package = "mocaredd")
+path <- system.file("extdata/example2-with-sims.xlsx", package = "mocaredd")
 
 cs <- read_xlsx(path = path, sheet = "c_stocks", na = "NA")
 ad <- read_xlsx(path = path, sheet = "AD_lu_transitions", na = "NA")
@@ -13,10 +13,10 @@ set.seed(1)
 res <- fct_combine_mcs_cstock(.ad = ad, .cs = cs, .usr = usr)
 
 test_res <- res |>
-  dplyr::filter(.data$sim_no == 1, .data$lu_id == "open") |>
-  dplyr::pull("C_all") |>
+  dplyr::filter(.data$sim_no == 1, .data$lu_id == "EV") |>
+  dplyr::pull("c_stock") |>
   round()
 
-testthat::test_that("fct_combine_mcs_C works on example1", {
-  testthat::expect_equal(test_res, 60)
+testthat::test_that("fct_combine_mcs_C works on example2", {
+  testthat::expect_equal(test_res, 136)
 })

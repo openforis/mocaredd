c06636766a81acca32ccaa3dac4d7163fd49a92c
Gael Sola <gaelsola@hotmail.fr>
2025-11-18 14:41:52 +0100

    added anonymous tracker

--- R/shiny_run_mocaredd.R
+++ R/shiny_run_mocaredd.R
@@ -149,20 +149,7 @@ shiny_run_mocaredd <- function(...) {
     shiny.i18n::usei18n(i18n),
     tags$head(
       # Load Plausible tracker
-      tags$script(
-        async = NA, defer = NA,
-        src = "https://plausible.io/js/script.js",
-        `data-domain` = "openforis-shiny.shinyapps.io/mocaredd"
-
-        # <!-- Privacy-friendly analytics by Plausible -->
-        #   <script async src="https://plausible.io/js/pa-8ulENHu4H88ja5ELIpSNg.js"></script>
-        #   <script>
-        #   window.plausible=window.plausible||function(){(plausible.q=plausible.q||[]).push(arguments)},plausible.init=plausible.init||function(i){plausible.o=i||{}};
-        # plausible.init()
-        # </script>
-
-
-        ),
+      tags$script(src="plausible.js"),
       # Custom event handler for Shiny + Plausible
       tags$script(HTML(js_plausible_event))
     ),
---
+++ plausible.js
@@ -0,0 +1,6 @@
+<!-- Privacy-friendly analytics by Plausible -->
+<script async src="https://plausible.io/js/pa-8ulENHu4H88ja5ELIpSNg.js"></script>
+<script>
+  window.plausible=window.plausible||function(){(plausible.q=plausible.q||[]).push(arguments)},plausible.init=plausible.init||function(i){plausible.o=i||{}};
+  plausible.init()
+</script>
